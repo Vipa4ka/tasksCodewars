19f9b9338da115df8f7978a56b89c942d0991806
Your Name <you@example.com>
2021-12-02 10:33:29 +0200

    3

---
+++ 3.js
@@ -0,0 +1,12 @@
+function solution(str, ending){
+  // TODO: complete
+}
+
+
+
+
+// Завершите решение, чтобы оно возвращало истину,если первый переданный аргумент(строка)
+//  заканчивается вторым аргументом(также строкой).
+// Примеры:
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
