a0eb3d47139b4f0c3bc762411f38bd7b129b7ae6
Your Name <you@example.com>
2021-12-16 11:16:21 +0200

    5

--- 5.js
+++ 5.js
@@ -2,7 +2,7 @@ function rot13(message){
   //your code here
 }
 
-console.log(rot13("jhgdfgdg"))
+console.log(rot13("hello"))
 
 
 // ROT13 - это простой шифр подстановки букв, который заменяет букву буквой из 13 букв после нее в алфавите. ROT13 - это пример шифра Цезаря.
