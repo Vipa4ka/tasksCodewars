bca5a0edf5908265df28fe7253bf99cd1e52c364
Your Name <you@example.com>
2022-01-19 22:31:04 +0200

    CW_part1

---
+++ 20.js
@@ -0,0 +1,12 @@
+function noSpace(x){
+return x.replace(/\s+/g, '');
+}
+
+
+
+
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
+
+
+// Просто удалите пробелы из строки,
+//  затем верните результирующую строку.
---
+++ 24.js
@@ -0,0 +1,18 @@
+function otherAngle(a, b) {
+  return (180-a-b);
+}
+
+console.log(otherAngle(30, 60));
+console.log(otherAngle(60, 60));
+console.log(otherAngle(43, 78));
+console.log(otherAngle(10, 20));
+
+
+
+// Вам даны два внутренних угла (в градусах) треугольника.
+
+// Напишите функцию, возвращающую 3-е число.
+
+// Примечание: будут проверены только положительные целые числа.
+
+// https://en.wikipedia.org/wiki/Треугольник
